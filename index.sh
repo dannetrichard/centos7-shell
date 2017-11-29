b234f8551ab9b0d9c89df04bf2224cac5ca7e1a7
dannetrichard <jinjun0927@126.com>
2017-11-30 07:32:22 +0800

    refresh

--- index.sh
+++ index.sh
@@ -5,7 +5,7 @@ mkdir -p /data/www/
 
 yum update -y
 yum install git -y
-git clone https://github.com/dannetrichard/init.git
+git clone https://github.com/dannetrichard/centos7-shell.git
 chmod +x ./centos7-shell/*.sh
 
 ./centos7-shell/lemp.sh
