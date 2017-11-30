02f45c3f477c70672e1c0a2956a0ab66ac4451ec
dannetrichard <jinjun0927@126.com>
2017-11-30 09:15:02 +0800

    composer

--- lemp.sh
+++ lemp.sh
@@ -60,3 +60,7 @@ systemctl enable php-fpm.service
 yum -y install yum-utils
 yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 sudo yum install certbot-nginx -y
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+composer config -g repo.packagist composer https://packagist.phpcomposer.com
